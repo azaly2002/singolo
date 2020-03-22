e9606f415be8eb618492d2aec45fd57fcf7aa98a
Azaly <emildautov22@gmail.com>
2020-03-22 15:50:01 +0600

    added js code for singolo dom

--- script.js
+++ script.js
@@ -1,47 +1,27 @@
-let b = document.getElementsByClassName('left')[0]
-let a = document.getElementsByClassName('right')[0]
-let pic1 = document.getElementById('img1')
-let pic2 = document.getElementById('img2')
+//slider
+let b = document.getElementsByClassName('left')[0];
+let a = document.getElementsByClassName('right')[0];
+a.addEventListener('click',() =>  Onclick())
+b.addEventListener('click', () => Onclick())
+function Onclick () {
+  let pic = Array.from(document.querySelectorAll('.img1'));
+      pic[1].classList.toggle('active')
+      pic[0].classList.toggle('active')
+    }
 
+//border
+let switchNow = true;
+const portfolioPic = portfolio.querySelectorAll(".pictures");
+portfolioPic.forEach(image => image.addEventListener("click", (event) => {
+  if ( event.target.classList.contains("bordered") ) {
+    switchNow = false;
+  }
 
-b.addEventListener('click', Onclickb)
-function Onclickb () {
+  portfolio.querySelectorAll("img").forEach(pic => pic.classList.remove("bordered"));
+  
+  if (switchNow) {
+    event.target.classList.add("bordered");
+  }
 
-    if (pic1.style.display == 'none') {
-        pic2 = pic2.style.display = 'none'
-        pic1 = pic1.style.display = 'flex'
-      } else {
-        pic1 = pic1.style.display = 'none'
-        pic2 =  pic2.style.display = 'flex'
-      }
-
-    // if(pic1.style.display === "none"){
-    //     pic2 = pic2.style.display = "flex"
-    // } else if (pic1.style.display = "flex") {
-    //     pic2 = pic2.style.display = "none"
-    // }
-
-}
-
-a.addEventListener('click', Onclicka)
-function Onclicka () {
-
-    if (pic1.style.display == 'none') {
-        pic2 = pic2.style.display = 'none'
-        pic1 = pic1.style.display = 'flex'
-      } else {
-        pic1 = pic1.style.display = 'none'
-        pic2 =  pic2.style.display = 'flex'
-      }
- 
-    // if(pic1.style.display === "flex"){
-    //     pic2 = pic2.style.display ="none"
-
-    // } else if (pic1.style.display = "flex"){
-    //     pic1 = pic1.style.display = "none"
-    // }
-
-}
-
-
-
+  switchNow = true;
+}));
